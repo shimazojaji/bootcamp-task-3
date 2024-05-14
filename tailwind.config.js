d4a37ccbf6dfe196725bd24b8b1536fe19eca994
shimazojaji <shima.zojaji@gmail.com>
2024-05-14 14:59:36 +0330

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ["Vazirmatn"],
+      },
+      colors: {
+        'header-color': '#3F4264',
+        'title-color':'#2A2D53',
+        'subtitle-color':'#555775', 
+        'price-color':'#4A6DFF',
+        'trash-color':'#FF0000CC',
+        'button-color':' #4A6DFF',
+
+
+      },
+    },
   },
   plugins: [],
 }
